da9889c621d8bf4a2e193f7d6d152e9fc9c30a1f
Ashfaq Haneef <78129119+mrthangu@users.noreply.github.com>
2023-12-06 12:56:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Server } from 'socket.io';
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://whatsapp.mrzera.xyz',
     },
 });
 
